ac9d92f07d8dbfcf41e727e4449326baa6426f7f
Silvia Rocha <silviadprocha@gmail.com>
2020-05-19 17:07:05 +0900

    add database.yml

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -4,5 +4,7 @@ class ProductsTest < ApplicationSystemTestCase
    test "visiting the index" do
     visit "/"
     assert_selector "h1", text: "Awesome Products"
+
+     save_and_open_screenshot
   end
 end
